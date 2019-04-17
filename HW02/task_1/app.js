a192e645c3d117be46d1f34229f1c166a27ce670
Dmitriy Kucher <dmitriykucher111@gmail.com>
2019-04-17 14:33:29 -0700

    1

--- HW02/task_1/app.js
+++ HW02/task_1/app.js
@@ -7,27 +7,18 @@ const numbers = [];
 do {
   input = prompt('Введите число:');
   inputNum = Number(input);
-  if (input > 0 || input < 0) {
+  if (input >= 0 || input < 0) {
     numbers.push(inputNum);
-  } else if (inputNum === 0) {
-    continue;
   } else {
     alert('Вы ввели не число. Попробуйте еще раз!');
     continue;
   }
-  console.log(numbers); //для наглядности
 } while (input !== null);
 
 let total = 0;
-let idx = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
-  total += numbers[idx];
-  idx += 1;
+  total += numbers[i];
 }
 
-if (numbers.length === 0) {
-  console.log('Вы не ввели ни одного числа.');
-} else {
-  console.log(`Общая сумма чисел равна: ${total}`);
-}
+console.log(`Общая сумма чисел равна: ${total}`);
